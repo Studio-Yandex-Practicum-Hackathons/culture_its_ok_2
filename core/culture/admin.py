6d72b0af0cfbeaf8dc257b54fb11ba3b62c6ccbd
Виктор Баранов <vitynnn@yandex.ru>
2023-08-16 10:02:52 +0300

    Added models in accordance with request

--- core/culture/admin.py
+++ core/culture/admin.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
 
-from .models import Review, Exhibit, FeedBack, Route
+from .models import Exhibit, FeedBack, Review, Route, RouteExhibit
 
 
-admin.site.register(Review)
-admin.site.register(Exhibit)
-admin.site.register(FeedBack)
-admin.site.register(Route)
+class ExhibitInline(admin.TabularInline):
+    model = RouteExhibit
+    min_num = 1
+    extra = 1
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'exhibit', 'text',)
+    search_fields = ['exhibit__name', 'username']
+    list_filter = ['exhibit', 'text', 'username']
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Exhibit)
+class ExhibitAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'name',)
+    search_fields = ['author', 'name']
+    list_filter = ['author', 'name']
+    empty_value_display = '-пусто-'
+
+
+@admin.register(FeedBack)
+class FeedBackAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'text',)
+    search_fields = ['username', 'text']
+    list_filter = ['username', 'text']
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
+    search_fields = ['name']
+    list_filter = ['name']
+    empty_value_display = '-пусто-'
+    inlines = (ExhibitInline,)
