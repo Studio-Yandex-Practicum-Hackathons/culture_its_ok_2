17e3547998e79213d11ec4347627c787e3a6c273
Алексей Боборыкин <alexey.boborykin@yandex.ru>
2023-08-23 22:02:55 +0300

    pdf report improvements

--- core/culture/models.py
+++ core/culture/models.py
@@ -1,23 +1,25 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from PIL import Image
 
 
 class PreBase(models.Model):
     """Родительская модель для маршрутов и экспонатов"""
+
     name = models.CharField(
         max_length=150,
-        verbose_name='Название',
-        default='Без названия',
+        verbose_name="Название",
+        default="Без названия",
     )
     description = models.TextField(
-        verbose_name='Описание',
-        default='Без описания',
+        verbose_name="Описание",
+        default="Без описания",
     )
     image = models.ImageField(
-        upload_to='pictures',
-        verbose_name='Фото',
-        )
-    address = models.TextField(verbose_name='Точный адрес')
+        upload_to="pictures",
+        verbose_name="Фото",
+    )
+    address = models.TextField(verbose_name="Точный адрес")
 
     def save(self, *args, **kwargs):
         """Изменяется раширение изображения?"""
@@ -27,7 +29,7 @@ class PreBase(models.Model):
             fixed_width = 1080
             filepath = self.image.path
             img = Image.open(filepath)
-            width_percent = (fixed_width / float(img.size[0]))
+            width_percent = fixed_width / float(img.size[0])
             height_size = int((float(img.size[1]) * float(width_percent)))
             new_image = img.resize((fixed_width, height_size))
             new_image.save(filepath)
@@ -41,154 +43,155 @@ class PreBase(models.Model):
 
 class Route(PreBase):
     """Модель для описания маршрутов"""
+
     route_map = models.ImageField(
-        upload_to='pictures',
-        verbose_name='Карта маршрута',
-        )
+        upload_to="pictures",
+        verbose_name="Карта маршрута",
+    )
     text_route_start = models.TextField(
-        verbose_name='Текст к месту начала маршрута',
+        verbose_name="Текст к месту начала маршрута",
         blank=True,
     )
     exhibite = models.ManyToManyField(
-        'Exhibit',
-        through='RouteExhibit',
-        related_name='routes',
+        "Exhibit",
+        through="RouteExhibit",
+        related_name="routes",
     )
 
     class Meta:
-        ordering = ['id']
-        verbose_name = 'Маршрут'
-        verbose_name_plural = 'Маршруты'
+        ordering = ["id"]
+        verbose_name = "Маршрут"
+        verbose_name_plural = "Маршруты"
 
     def __str__(self):
-        return f'Маршрут {self.pk}'
+        return f"Маршрут {self.pk}"
 
 
 class Exhibit(PreBase):
     """Модель для описания объектов"""
+
     author = models.CharField(
         max_length=100,
-        verbose_name='Автор',
-        default='Не указан',
+        verbose_name="Автор",
+        default="Не указан",
     )
     how_to_pass = models.TextField(
-        verbose_name='Путь до объекта',
+        verbose_name="Путь до объекта",
         blank=True,
-        )
+    )
     message_before_description = models.TextField(
-        verbose_name='Подводка',
+        verbose_name="Подводка",
         blank=True,
-        )
+    )
     reflection = models.TextField(
-        verbose_name='Рефлексии',
+        verbose_name="Рефлексии",
         blank=True,
-        )
+    )
     reflection_positive = models.TextField(
-        verbose_name='Сообщение после положительного ответа пользователя',
+        verbose_name="Сообщение после положительного ответа пользователя",
         blank=True,
-        )
+    )
     reflection_negative = models.TextField(
-        verbose_name='Сообщение после отрицательного ответа пользователя',
+        verbose_name="Сообщение после отрицательного ответа пользователя",
         blank=True,
-        )
+    )
     transfer_message = models.TextField(
-        verbose_name='Сообщение для перехода к следующему объекту',
+        verbose_name="Сообщение для перехода к следующему объекту",
         blank=True,
-        )
+    )
 
     class Meta:
-        ordering = ['id']
-        verbose_name = 'Объект'
-        verbose_name_plural = 'Объекты'
+        ordering = ["id"]
+        verbose_name = "Объект"
+        verbose_name_plural = "Объекты"
 
     def __str__(self):
-        return f'Объект {self.pk}: {self.name}'
+        return f"Объект {self.pk}: {self.name}"
 
 
-class RouteExhibit (models.Model):
+class RouteExhibit(models.Model):
     route = models.ForeignKey(
         Route,
         on_delete=models.CASCADE,
-        verbose_name='Маршрут',
+        verbose_name="Маршрут",
     )
     exhibit = models.ForeignKey(
         Exhibit,
         on_delete=models.CASCADE,
-        verbose_name='Объекты',
+        verbose_name="Объекты",
     )
 
     class Meta:
-        verbose_name = 'Объект'
-        verbose_name_plural = 'Объекты'
+        verbose_name = "Объект"
+        verbose_name_plural = "Объекты"
         constraints = [
             models.UniqueConstraint(
-                fields=['route', 'exhibit'],
-                name='unique_exhibites_route'
+                fields=["route", "exhibit"], name="unique_exhibites_route"
             )
         ]
 
     def __str__(self):
-        return f'Объект {self.exhibit.pk}'
+        return f"Объект {self.exhibit.pk}"
 
 
 class Review(models.Model):
     """Модель для отзывов пользователя"""
+
     username = models.CharField(
         max_length=100,
-        verbose_name='Имя',
-        default='Аноним',
+        verbose_name="Имя",
+        default="Аноним",
     )
     userage = models.IntegerField(
-        blank=True,
-        null=True,
-        verbose_name='Возраст'
-        )
+        blank=True, null=True, verbose_name="Возраст"
+    )
     userhobby = models.CharField(
         max_length=150,
-        verbose_name='Хобби',
-        default='Не указано',
+        verbose_name="Хобби",
+        default="Не указано",
     )
     exhibit = models.ForeignKey(
         Exhibit,
         on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Объект',
+        related_name="reviews",
+        verbose_name="Объект",
     )
     answer_to_message_before_description = models.TextField(
-        verbose_name='Ответ на подводку',
-        default='Вопроса не было.',
+        verbose_name="Ответ на подводку",
+        default="Вопроса не было.",
         blank=True,
         null=True,
     )
     answer_to_reflection = models.TextField(
-        verbose_name='Ответ на рефлексию',
-        default='Ответа не было.',
+        verbose_name="Ответ на рефлексию",
+        default="Ответа не было.",
         blank=True,
         null=True,
     )
-    text = models.TextField(verbose_name='Текст отзыва')
+    text = models.TextField(verbose_name="Текст отзыва")
 
     class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
 
     def __str__(self):
-        return f'Отзыв {self.pk}'
+        return f"Отзыв {self.pk}"
 
 
 class FeedBack(models.Model):
     """Модель для опросов пользователя"""
+
     email = models.EmailField(max_length=254, unique=True)
     route = models.ForeignKey(
         Route,
         on_delete=models.CASCADE,
-        verbose_name='Маршрут',
+        verbose_name="Маршрут",
     )
-    text = models.TextField(verbose_name='Отзыв на маршрут')
+    text = models.TextField(verbose_name="Отзыв на маршрут")
 
     class Meta:
-        verbose_name = 'Опрос'
-        verbose_name_plural = 'Опросы'
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
 
     def __str__(self):
-        return f'Опрос {self.pk}'
+        return f"Опрос {self.pk}"
--- core/culture/utils.py
+++ core/culture/utils.py
@@ -3,13 +3,22 @@ from datetime import datetime
 from io import BytesIO
 
 from django.conf import settings
+from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import Paragraph, SimpleDocTemplate
 from reportlab.platypus.tables import Table, TableStyle, colors
 
-TABLE_HEADER = ["Username", "Возраст", "Текст отзыва"]
+TABLE_HEADER = [
+    "Имя",
+    "Возраст",
+    "Хобби",
+    "Объект",
+    "Ответ на подводку",
+    "Ответ на рефлексию",
+    "Текст отзыва",
+]
 
 styles = getSampleStyleSheet()
 
@@ -25,23 +34,37 @@ def generate_pdf(data):
     """Создание отчета в формате .pdf"""
 
     buffer = BytesIO()
-    report = SimpleDocTemplate(buffer)
+    report = SimpleDocTemplate(buffer, pagesize=landscape(A4))
 
     styles = getSampleStyleSheet()
     header_style = styles["Heading2"]
 
     elements = []
 
-    header = Paragraph(f"Отчет на {datetime.now()}", header_style)
+    header = Paragraph(f"Отзывы, {datetime.now()}", header_style)
     elements.append(header)
 
     table_header = TABLE_HEADER.copy()
     table_content = [
-        map(str, [obj.username, obj.userage, obj.text]) for obj in data
-        ]
+        map(
+            str,
+            [
+                obj.username,
+                obj.userage,
+                obj.userhobby,
+                obj.exhibit,
+                obj.answer_to_message_before_description,
+                obj.answer_to_reflection,
+                obj.text,
+            ],
+        )
+        for obj in data
+    ]
     table_data = [table_header, *table_content]
     table = Table(table_data)
-    table.setStyle(TableStyle([("BACKGROUND", (0, 0), (-1, 0), colors.aqua)]))
+    table.setStyle(
+        TableStyle([("BACKGROUND", (0, 0), (-1, 0), colors.dimgrey)])
+    )
     elements.append(table)
 
     report.build(elements)
