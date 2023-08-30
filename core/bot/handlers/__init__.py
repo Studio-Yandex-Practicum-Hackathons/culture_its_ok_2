5968e2d5a02dfc0b9d22c0e82500c30e62ee2b6d
Арсений Изимов <arsenyxiii@gmail.com>
2023-08-30 16:10:29 +0300

    fixs

--- core/bot/handlers/__init__.py
+++ core/bot/handlers/__init__.py
@@ -1,2 +1,2 @@
-from handlers.meetings import meetings_router  # noqa
-from handlers.route import route_router  # noqa
+from bot.handlers.meetings import meetings_router  # noqa
+from bot.handlers.route import route_router  # noqa
